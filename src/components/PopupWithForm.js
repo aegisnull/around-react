726e7f0bd96c57b35286d70a25690b681f2b06ce
Luis Tellez <luis@luistellez.com>
2022-10-09 11:41:52 -0700

    feat: apply DRY to PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,1 +1,16 @@
-function PopupWithForm(props) {}
+function PopupWithForm(props) {
+  return (
+    <div className={`modal modal-${props.name}`}>
+      <div className="modal__container">
+        <h2 className="modal__title">{props.title}</h2>
+        <form
+          className={`modal__form modal__form_${props.name}`}
+          name={props.name}
+        >
+          {props.children}
+        </form>
+        <button className="modal__close-button modal__close"></button>
+      </div>
+    </div>
+  );
+}
