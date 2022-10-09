7128c3f4f70dbeb22b168fd57f36615a55255e74
Luis Tellez <luis@luistellez.com>
2022-10-09 14:03:30 -0700

    feat: init ImagePopup.js

---
+++ src/components/ImagePopup.js
@@ -0,0 +1,11 @@
+function ImagePopup() {
+  return (
+    <div id="lightbox" class="popup">
+      <img class="popup__image" src=" " alt=" " />
+      <p class="popup__title"></p>
+      <button class="popup__close"></button>
+    </div>
+  );
+}
+
+export default ImagePopup;
