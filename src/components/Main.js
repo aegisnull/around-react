5ba3ef41230c8124aec5dbd3156e5bb401f0efbb
Luis Tellez <luis@luistellez.com>
2022-10-08 20:42:00 -0700

    feat: declare imperative handlers for modal open

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,18 @@ import edit from "../images/edit.svg";
 import post from "../images/post.svg";
 
 function Main() {
+  function handleEditAvatarClick() {
+    document.querySelector(".modal-profile").classList.add("modal_active");
+  }
+
+  function handleEditProfileClick() {
+    document.querySelector(".modal").classList.add("modal_active");
+  }
+
+  function handleAddPlaceClick() {
+    document.querySelector(".modal-post").classList.add("modal_active");
+  }
+
   return (
     <main className="main__container">
       <section className="profile">
