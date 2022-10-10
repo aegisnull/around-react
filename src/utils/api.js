2b083ff5ef332184d1982ac38b13b1744bcd3cf5
Luis Tellez <luis@luistellez.com>
2022-10-09 23:49:32 -0700

    feat: init api.js

---
+++ src/utils/api.js
@@ -0,0 +1,5 @@
+const api = new Api({
+    address: 'https://around.nomoreparties.co/v1/',
+    groupId: `cohort-1-es`, // CHANGE IT WITH YOUR COHORT
+    token: `e7cf5ec1-f874-45f0-bd41-d890ac5955db`, // CHANGE IT WITH YOUR TOKEN
+  });"    
