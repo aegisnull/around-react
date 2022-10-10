fd437c5bd695fd72ed77c3547906cbfaaa81d07a
Luis Tellez <luis@luistellez.com>
2022-10-10 10:57:27 -0700

    fix: remove old card template code

--- src/components/App.js
+++ src/components/App.js
@@ -49,20 +49,6 @@ function App() {
         isOpen={isEditAvatarPopupOpen}
         onClose={closeAllPopups}
       />
-
-      <template id="cards">
-        <div className="card__container">
-          <button className="card__remove-button"></button>
-          <img className="card__img" src=" " alt=" " />
-          <div className="card__text-container">
-            <h2 className="card__title">" "</h2>
-            <div className="card__like-container">
-              <button className="card__like-button"></button>
-              <span className="card__like-counter">1</span>
-            </div>
-          </div>
-        </div>
-      </template>
     </div>
   );
 }
