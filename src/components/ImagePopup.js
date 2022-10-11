b0ddf6440c9f23323e643a78535bdabdb8190d90
Luis Tellez <luis@luistellez.com>
2022-10-10 23:21:36 -0700

    feat: add ImagePopup on card click

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,9 +1,9 @@
 function ImagePopup({ isOpen, card, onClose }) {
   return (
     <div id="lightbox" className={`popup ${isOpen ? "active" : ""}`}>
-      <img className="popup__image" src=" " alt=" " />
-      <p className="popup__title"></p>
-      <button className="popup__close"></button>
+      <img className="popup__image" src={card.link} alt={card.name} />
+      <p className="popup__title">{card.name}</p>
+      <button className="popup__close" onClick={onClose}></button>
     </div>
   );
 }
