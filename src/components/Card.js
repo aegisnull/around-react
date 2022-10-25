4cf837e3faf04d22c18bc303a7dce0e255939fc2
Luis Tellez <luis@luistellez.com>
2022-10-25 09:37:24 -0700

    react: set code to enable delete card button if id matches creator

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,29 @@
+import { useContext } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+
 function Card({ cardData, onCardClick }) {
+  const currentUser = useContext(CurrentUserContext);
+  // Verificando si el usuario actual es el propietario de la tarjeta actual
+  const isOwn = cardData.owner._id === currentUser._id;
+
+  // Creando una variable que después establecerás en `className` para el botón eliminar
+  const cardDeleteButtonClassName = `card__remove-button ${
+    isOwn ? "card__remove-button_active" : "card__remove-button"
+  }`;
+
+  /*   // Verifica si el usuario actual le dio "like" a la tarjeta
+  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+
+  // Crea una variable que después establecerás en `className` para el botón like
+  const cardLikeButtonClassName = `...`; */
+
   function handleClick() {
     onCardClick(cardData);
   }
 
   return (
     <div className="card">
-      <button className="card__remove-button" />
+      <button className={cardDeleteButtonClassName} />
       <img
         className="card__img"
         src={cardData.link}
